f0b6c695696bd60657e8bb40adc3479c7f3e5d60
Gravitysause <57376726+Gravitysause@users.noreply.github.com>
2022-03-08 11:10:48 -0500

    Implementing the ability to view ballence

--- dataBase.py
+++ dataBase.py
@@ -10,10 +10,23 @@ dataClient = MongoClient(os.getenv("mongo"))
 db = dataClient.users
 collection = db.userCollection
 
-def createAccount(name, balence):
-    user = {
-        "name": name,
-        "balence": balence
-    }
 
+def createAccount(ID):
+    """
+    Creates an object on mongodb for the specified user
+    Each new account will start with 100 dollars
+    """
+
+    user = {
+        "discordID": ID,
+        "ballence": 100
+    }
+    
     collection.insert_one(user)
+
+
+def viewBallence(ID):
+    userObject = collection.find_one()
+
+    if userObject["discordID"] == ID:
+        return userObject["ballence"]
--- market-place.py
+++ market-place.py
@@ -9,23 +9,37 @@ dotenv.load_dotenv()
 # Discord CLient Key
 token = os.getenv("token")
 
+# The Bot
 class MarketPlace(discord.Client):
     async def on_message(self, message):
-
         # Prevents bot from responding to its self
         if message.author == self.user:
             return
 
+        elif message.content.startswith("$running"):
+            await message.channel.send("Yes")
+
         # creates account
         elif message.content.startswith("$create"):
-            dataBase.createAccount(str(message.author), 100)
+            dataBase.createAccount(message.author.id)
 
-        # view account
+            await message.channel.send(f"Creating an account for {message.author}")
+
+        # prints ballence
         elif message.content.startswith("$view"):
-            print("Viewing Account")
+            ballence = dataBase.viewBallence(message.author.id)
 
-            await message.channel.send(f"{message.author} has a balence of {message.author}")
+            if ballence == None:
+                await message.channel.send(f"{message.author} doesn't have an account. \n to create an account, use $create")
+
+            else: 
+                await message.channel.send(f"{message.author} has ${ballence}")
 
 
-discordClient = MarketPlace()
-discordClient.run(token)
+if __name__ == "__main__":
+    try:
+        discordClient = MarketPlace()
+        discordClient.run(token)
+
+    except:
+        print("Invalid Bot Token")
